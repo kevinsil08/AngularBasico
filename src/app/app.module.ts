4f78c2198509dc9d38b6a9348b46e967c2841795
Kevin Campoverde <008kevinsil@gmail.com>
2022-08-17 16:14:03 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorModule } from './contador/contador.module';
+import { heroesModule } from './heroes/heroes.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    heroesModule,
+  ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
